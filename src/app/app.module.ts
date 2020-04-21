7468650779d221bd76713a5df7809f06a0736835
DegamSathishReddy <49183292+DegamSathishReddy@users.noreply.github.com>
2020-04-21 13:21:28 +0530

    Practice Day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    NavBarComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
